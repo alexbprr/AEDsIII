49afb6da012d5aaba0b625372a027684ec0a07fb
alexbprr <alexbprr@gmail.com>
2023-04-20 13:49:56 -0300

    pd

--- ProgramacaoDinamica/selecaoAtividadesPD.c
+++ ProgramacaoDinamica/selecaoAtividadesPD.c
@@ -22,6 +22,8 @@ void imprimeAtividades(atividades **a, int i, int j){
 int selecaoAtividadesPD(atividades **a, int s[], int f[], int i, int j){
     if (j - i < 2)
         return 0;
+    if (a[i][j].c > 0)
+        return a[i][j].c;
     for(int k=i+1; k < j; k++){
         a[i][k].c = selecaoAtividadesPD(a,s,f,i,k);
         a[k][j].c = selecaoAtividadesPD(a,s,f,k,j);
@@ -34,10 +36,10 @@ int selecaoAtividadesPD(atividades **a, int s[], int f[], int i, int j){
 }
 
 int main(){
-    int s[] =  {0, 1, 3, 0, 4, 3, 5, 6, 8, 8, 2, 12, 100};
-    int f[] =  {0, 4, 5, 6, 6, 8, 9, 10, 11, 12, 13, 14, 200};
-    //int s[] =  {0, 1, 3, 0, 4, 3, 100};
-    //int f[] =  {0, 5, 5, 6, 7, 8, 200};
+    //int s[] =  {0, 1, 3, 0, 4, 3, 5, 6, 8, 8, 2, 12, 100};
+    //int f[] =  {0, 4, 5, 6, 6, 8, 9, 10, 11, 12, 13, 14, 200};
+    int s[] =  {0, 1, 3, 0, 4, 3, 100};
+    int f[] =  {0, 5, 5, 6, 7, 8, 200};
     int n = sizeof(s)/sizeof(s[0]);
     printf("%d\n", n);
     atividades **ativ = (atividades**)malloc((n)*sizeof(atividades*));
@@ -52,14 +54,14 @@ int main(){
         }
 
     int maxAct = selecaoAtividadesPD(ativ, s, f, 0, n-1);
-    printf("Máximo de atividades compatíveis: %d\n", maxAct);
+    printf("Maximo de atividades compativeis: %d\n", maxAct);
     printf("Atividades selecionadas:\n");    
     imprimeAtividades(ativ,0,n-1);
 
-    printf("\nc:\n");
-    for (int i=0; i < n; i++) {
-        for (int j=i+2; j < n; j++)
-            printf("c[%d,%d] = %d; ", i, j, ativ[i][j].c);
+    printf("\nCustos:\n");
+    for (int i=0; i < n - 1; i++) {
+        for (int j=i+1; j < n; j++)
+            printf("c[%d,%d] = %d;\n", i, j, ativ[i][j].c);
         printf("\n");
     }
 
